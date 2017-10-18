36a8a789e6e2452dd4fe132d2b06873a7e753a77
Noopur Maheshwari <noopur2812@gmail.com>
2017-10-18 04:38:53 -0400

    72, 10 working .. 55, 10 infinite
    Signed-off-by: Noopur Maheshwari <noopur2812@gmail.com>

--- PROJECT3/submit.py
+++ PROJECT3/submit.py
@@ -16,10 +16,22 @@ def check_constraints(V, L, M):
                 return 0
     return 1
 
-def is_consistent(d, V,dist):
+def is_consistent(d, V, addist, Dist):
     #print "d-c", d
     #print "V-c", V
     #print "dist-c", dist
+    
+    for i in range(0, len(V)):
+        di = abs(d - V[i])
+        if di in Dist:
+            addist = []
+            return 0
+        else :
+            addist.append(di)
+    
+    return 1
+
+"""
     if (len(V) == 0):
         return 1
     V.append(d)
@@ -33,6 +45,7 @@ def is_consistent(d, V,dist):
                 return 0
     V.remove(d)
     return 1
+"""
 
 def forward_check(V,D,dist,rem):
     #print "dist:",dist
@@ -44,7 +57,7 @@ def forward_check(V,D,dist,rem):
                 rem.append(di)
                 D.remove(di)
 
-def rec_BT(L, M, V, D):
+def rec_BT(L, M, V, D, Dist):
     #print "V:",V
     #print "D:",D
     if (check_constraints(V, L, M)):
@@ -53,9 +66,9 @@ def rec_BT(L, M, V, D):
     #if (len(V < (M - 1)))
     for d in D :
         #print "d:",d
-        dist = []
+        adddist = []
         rem = []
-        if (is_consistent(d, V,dist)):
+        if (is_consistent(d, V, adddist, Dist)):
             # Assign value to a variable from domain
             V.append(d)
             # Remove d from possible Domains
@@ -63,22 +76,31 @@ def rec_BT(L, M, V, D):
             D.remove(d)
             #print "V1:",V
             #print "D1:",D
-            forward_check(V,D,dist,rem)
+            #forward_check(V,D,dist,rem)
             #print "D:",D
             #print "rem:",rem
 
             # Call recursively to assign further variables from the domain
-            if rec_BT(L, M, V, D) :
+            for i in adddist :
+                Dist.append(i)
+
+            if rec_BT(L, M, V, D, Dist) :
                 return 1
             #print "BT:V1:",V
             #print "BT:D:",D
             #print "BT:rem:",rem
-            for di in rem:
-                    D.append(di)
+            #for di in rem:
+            #        D.append(di)
             # Remove assigned value to the variable
+        
+            for i in adddist :
+                Dist.remove(i)
+
             V.remove(d)
             # Add the domain to possible domains
             D.append(d)
+
+
     
       
     return 0
@@ -87,13 +109,15 @@ def rec_BT(L, M, V, D):
 def BT(L, M):
     domain = []
     var = []
+    dist = []
     "*** YOUR CODE HERE ***"
     var.append(0)
     var.append(L)
+    dist.append(L)
     for i in range(1, L):
         domain.append(i)
         
-    rec_BT(L, M, var, domain)
+    rec_BT(L, M, var, domain, dist)
     var.sort()
     print var
     return -1,[]
@@ -112,6 +136,6 @@ def CP(L, M):
 print "Hello "
 import time
 ts = time.time()
-BT (55, 10)
+BT (72, 11)
 ts1 = time.time()
 print ts1-ts

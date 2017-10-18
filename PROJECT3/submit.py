ddc1f0c6b864a73268be73c84f3da2a27071afac
Siddesh Shinde <sshindesiddesh@gmail.com>
2017-10-18 15:59:28 -0500

    55, 10 runs in 12 sec.. but still problem with 72, 11

--- PROJECT3/submit.py
+++ PROJECT3/submit.py
@@ -3,17 +3,12 @@
 #Each of your functions should return the minimum possible L value alongside the marker positions
 #Or return -1,[] if no solution exists for the given L
 def check_constraints(V, L, M):
-    if (not V or len(V) != M or (L not in V)):
-        return 0
-    
-    dist = []
-    for i in range(0, M):
-        for j in range(i + 1, M):
-            d = abs(V[j] - V[i])
-            if d not in dist:
-                dist.append(d);
-            else :
-                return 0
+    if (not V):
+        return 0;
+    if (len(V) != M):
+        return 0;
+    if (len(V) > (len(set(V)))):
+        return 0;
     return 1
 
 def is_consistent(d, V, addist, Dist):
@@ -23,74 +18,58 @@ def is_consistent(d, V, addist, Dist):
     
     for i in range(0, len(V)):
         di = abs(d - V[i])
-        if di in Dist:
+        if di in Dist or di in addist:
             addist = []
             return 0
         else :
             addist.append(di)
+    return 1
+
+def forward_check(d, V, D, adddist, rem):
+    #print "FC:adddist:",adddist
+    #print "FC:D:",D
+    #print "FC:V:",V
     
-    return 1
-
-"""
-    if (len(V) == 0):
-        return 1
-    V.append(d)
-    for i in range(0, len(V)):
-        for j in range(i + 1, len(V)):
-            di = abs(V[j] - V[i])
-            if di not in dist:
-                dist.append(di);
-            else :
-                V.remove(d)
-                return 0
-    V.remove(d)
-    return 1
-"""
-
-def forward_check(V,D,dist,rem):
-    #print "dist:",dist
-    #print "D:",D
-    for i in range(0,len(V)):
-        for j in range(0,len(dist)):
-            di = V[i]+dist[j]
-            if di in D:
-                rem.append(di)
-                D.remove(di)
+    for v in V:
+        for di in adddist:
+            if ((v + di) in D):
+                rem.append(v + di)
+                D.remove(v + di)
 
 def rec_BT(L, M, V, D, Dist):
-    #print "V:",V
-    #print "D:",D
+    print "V:",V
+    print "D:",D
     if (check_constraints(V, L, M)):
         return 1
     # condition to check if all the variables are assigned
     #if (len(V < (M - 1)))
     for d in D :
-        #print "d:",d
+        print "d:",d
         adddist = []
         rem = []
         if (is_consistent(d, V, adddist, Dist)):
             # Assign value to a variable from domain
             V.append(d)
             # Remove d from possible Domains
-
             D.remove(d)
-            #print "V1:",V
-            #print "D1:",D
-            #forward_check(V,D,dist,rem)
-            #print "D:",D
-            #print "rem:",rem
-
+            
             # Call recursively to assign further variables from the domain
-            for i in adddist :
+            for i in adddist:
                 Dist.append(i)
 
-            forward_check(V, D, Dist, rem)
+            print "Dist 1:", Dist
+            print "V1:",V
+            print "D1:",D
+            forward_check(d, V, D, adddist, rem)
+            print "D:",D
+            print "rem:",rem
 
-            if rec_BT(L, M, V, D, Dist) :
-                return 1
-            #print "BT:V1:",V
-            #print "BT:D:",D
-            #print "BT:rem:",rem
+            if (len(V) + len(D)) >= M :
+                if rec_BT(L, M, V, D, Dist):
+                    return 1
+            print "BT:V1:",V
+            print "BT:D:",D
+            print "BT:rem:",rem
             for di in rem:
                     D.append(di)
             # Remove assigned value to the variable
@@ -138,6 +117,6 @@ def CP(L, M):
 print "Hello "
 import time
 ts = time.time()
-BT (72, 11)
+BT (11, 5)
 ts1 = time.time()
 print ts1-ts
